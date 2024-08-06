2459f8ceb3ac4a16ac697a4a0b2f01c4595b7509
nev <sshabalin3@gatech.edu>
2024-08-06 23:03:47 +0000

    It's slow now!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(array_chunks)]
 #![feature(portable_simd)]
+use rand_distr::Uniform;
 use rand::Rng;
-use std::simd::u32x64;
 
 use rayon::prelude::*;
 use half::bf16;
@@ -10,54 +10,41 @@ use half::bf16;
 const N: usize = 1 << 20;
 const K: usize = 64;
 const M: usize = 1 << 14;
-
+const RANDOM_CHUNK: usize = 1 << 14;
 
 #[inline]
-fn generate_floats<const S: usize>(generate_into: &mut [bf16; S]) {
-    generate_into.as_parallel_slice_mut().par_chunks_mut(16384).for_each(|x| {
+fn generate_weights<const S: usize>(generate_into: &mut [bf16; S], scale: f32) {
+    let distribution = Uniform::new(-scale, scale);
+    generate_into.as_parallel_slice_mut().par_chunks_mut(RANDOM_CHUNK).for_each(|x| {
         let mut rng = rand::thread_rng();
-        let mut xoroshiro_state: u32x64 = u32x64::from_array([0; 64].map(|_| rng.gen()));
 
-        let mask_gen = 0b100011_1111111111u32;
-        let mask = u32x64::splat(mask_gen);
-        let add_gen = 0b101100u32 << 10;
-        let add = u32x64::splat(add_gen);
-
-        x.array_chunks_mut::<64>().for_each(|y| {
-            xoroshiro_state ^= xoroshiro_state << 13;
-            xoroshiro_state ^= xoroshiro_state >> 17;
-            xoroshiro_state ^= xoroshiro_state << 5;
-            let generated_bf16s = ((xoroshiro_state & mask) | add).to_array();
-
-            y.iter_mut().enumerate().for_each(|(i, z)| {
-                *z = bf16::from_bits(generated_bf16s[i] as u16);
-            });
+        x.iter_mut().for_each(|y| {
+            *y = bf16::from_f32(rng.sample(distribution));
         });
     });
 }
 
-// #[inline]
-// fn generate_floats<const S: usize>(generate_into: &mut [bf16; S]) {
-//     generate_into.as_parallel_slice_mut().par_chunks_mut(16384).for_each(|x| {
-//         let mut rng = rand::thread_rng();
+#[inline]
+fn generate_indices<const S: usize>(generate_into: &mut [u32; S], max_value: u32) {
+    let distribution = Uniform::new(0, max_value);
+    generate_into.as_parallel_slice_mut().par_chunks_mut(RANDOM_CHUNK).for_each(|x| {
+        let mut rng = rand::thread_rng();
 
-//         x.iter_mut().for_each(|y| {
-//             *y = bf16::from_f32(rng.gen());
-//         });
-//     });
-// }
-
-// #[inline]
-// fn generate_floats<const S: usize>(generate_into: &mut [bf16; S]) {
-//     let mut rng = rand::thread_rng();
-//     generate_into.iter_mut().for_each(|y| {
-//         *y = bf16::from_f32(rng.gen());
-//     });
-// }
+        x.iter_mut().for_each(|y| {
+            *y = rng.sample(distribution);
+        });
+    });
+}
 
 
 fn main() {
-    let mut sparse_weights: Box<[bf16; N * K]> = vec![bf16::from_f32(0.0); N * K].into_boxed_slice().try_into().unwrap();
-    generate_floats(&mut sparse_weights);
-    println!("Generated floats: {:?}", &sparse_weights[0..10]);
+    let mut sparse_weights: Box<[bf16; N * K]> = vec![bf16::default(); N * K].into_boxed_slice().try_into().unwrap();
+    generate_weights(&mut sparse_weights, 50.0);
+
+    let mut sparse_indices: Box<[u32; N * K]> = vec![0; N * K].into_boxed_slice().try_into().unwrap();
+    generate_indices(&mut sparse_indices, M as u32);
+
+    let mut decoder_weights: Box<[bf16; N * M]> = vec![bf16::default(); N * M].into_boxed_slice().try_into().unwrap();
+    let scale = 1.0 / (M as f32).sqrt();
+    generate_weights(&mut decoder_weights, scale);
 }
